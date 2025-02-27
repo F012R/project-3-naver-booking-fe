b01fa75d0e0f681ba2fc7c1a4370cb72fe417e29
Jang Eunsun <20211234@duksung.ac.kr>
2025-02-27 23:58:01 +0900

    [NB-10] #9 ✨ Feat: 프로모션 배너 연결 및 에러 메시지 추가

--- public/index.js
+++ public/index.js
@@ -1,10 +1,54 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("../pages/main/promotion.html")
+  loadPromotionComponent()
+    .then(() => {
+      try {
+        loadPromotionBanner();
+      } catch (error) {
+        console.error("프로모션 배너 로딩 중 오류 발생:", error);
+      }
+    })
+    .catch((error) => console.error("프로모션 HTML 로딩 중 오류 발생:", error));
+});
+
+function loadPromotionComponent() {
+  return fetch("../pages/main/promotion.html")
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("promotion").innerHTML = data;
-    })
-    .catch((error) =>
-      console.error("promotion.html 로딩 중 오류 발생:", error)
-    );
-});
+    });
+}
+
+function loadPromotionBanner() {
+  const bannerContainer = document.querySelector(".promotion-banner");
+  const slider = bannerContainer.querySelector(".slider");
+  const image = slider.querySelectorAll(".slide-item");
+  let currentImageIndex = 0;
+
+  startBannerAnimation(slider, image, currentImageIndex);
+}
+
+function startBannerAnimation(slider, image, currentImageIndex) {
+  function moveToNextSlide() {
+    currentImageIndex++;
+    if (currentImageIndex >= image.length - 1) {
+      resetSliderPosition();
+    } else {
+      updateSliderPosition();
+    }
+  }
+
+  function resetSliderPosition() {
+    currentImageIndex = 0;
+    slider.style.transition = "none";
+    slider.style.transform = "translateX(0)";
+    setTimeout(() => {
+      slider.style.transition = "transform 0.5s ease";
+    }, 50);
+  }
+
+  function updateSliderPosition() {
+    slider.style.transform = `translateX(-${currentImageIndex * 50}%)`;
+  }
+
+  setInterval(moveToNextSlide, 3000);
+}
