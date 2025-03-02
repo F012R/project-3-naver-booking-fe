0fb1ae1bce71f551323b637131c658239585246d
Jang Eunsun <20211234@duksung.ac.kr>
2025-03-02 18:59:47 +0900

    [NB-13] #1 ♻️ Refactor: 컴포넌트 삽입용 함수 반영

--- public/index.js
+++ public/index.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("../pages/main/footer.html")
+  loadComponent("footer", "../pages/main/footer.html");
+});
+
+function loadComponent(id, url, callback) {
+  fetch(url)
     .then((response) => response.text())
     .then((data) => {
-      document.getElementById("footer").innerHTML = data;
+      document.getElementById(id).innerHTML = data;
+      if (callback) callback();
     })
-    .catch((error) => console.error("footer.html 로딩 중 오류 발생:", error));
-});
+    .catch((error) => console.error(`${url} 로딩 중 오류 발생:`, error));
+}
