4805c3598264e78456d9255bcc6aed1b91b34c47
hj9023 <hy9458@duksung.ac.kr>
2025-03-06 19:14:09 +0900

    ✨ Feat:  예매하기 버튼 구현 및 컴포넌트 연결 #14

--- pages/product-detail/product-detail.js
+++ pages/product-detail/product-detail.js
@@ -8,11 +8,12 @@ document.addEventListener("DOMContentLoaded", function () {
   loadComponent("footer", "../main/footer.html");
 });
 
-function loadComponent(id, url) {
+function loadComponent(id, url, callback) {
   fetch(url)
     .then((response) => response.text())
     .then((data) => {
       document.getElementById(id).innerHTML = data;
+      if (callback) callback();
     })
     .catch((error) => console.error(`${url} 로딩 중 오류 발생:`, error));
 }
